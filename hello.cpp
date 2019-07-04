f472b2460d3176fd34622f825e82ede756722d50
Linda DuHadway <52508271+lindawsu@users.noreply.github.com>
2019-07-04 09:55:13 -0600

    Initial code

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/* CS 1410 Module 0 Getting Started
+ * This program prints out the message
+ * Hello! Welcome to CS 1410!
+ */
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	cout<<"Hello!\n"
+		  <<"Welcome to CS 1410!"<<endl;
+
+	return 0;
+}
